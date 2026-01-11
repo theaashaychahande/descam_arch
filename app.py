cdb23a54f9c8865c308570d90e534d776fa4d4a9
Aashay Chahande <aashaychahande1717@gmail.com>
2026-01-11 17:37:40 +0530

    Change app.run to use host and port from environment

--- app.py
+++ app.py
@@ -352,4 +352,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
